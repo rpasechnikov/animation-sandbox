c4be8fc8af0639eab21bec4a8d3206adf4e4049c
Rostislav Pasechnikov <ross.pasechnikov@hammertech.com>
2025-11-03 08:55:04 +1100

    Basic slide in / slide out logic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import React, { useMemo, useState } from 'react';
+import classNames from 'classnames';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isVisible, setIsVisible] = useState(false);
+  const [animationDirection, setAnimationDirection] = useState(null);
+
+  const animate = () => {
+    setIsVisible(isVisible => !isVisible);
+  };
+
+  const contentClassNames = classNames(
+    'content',
+    { 
+      'visible': isVisible,
+      'hidden': !isVisible
+    }
+  );
 
   return (
-    <>
-      Hello World
-    </>
-  )
+    <div className='App'>
+      <button onClick={animate}>Animate</button>
+      <div className={contentClassNames}>Content</div>
+    </div>
+  );
 }
 
 export default App
